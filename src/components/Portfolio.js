5a0c23fe595f42ec92512e2f6a037f18d6ab434a
misiekmardosz <michal.mardosz@gmail.com>
2022-05-04 19:49:34 +0200

    portfolio section done with blure on projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Portfolio = () => {
     return(
@@ -6,16 +7,24 @@ const Portfolio = () => {
             <h2 className="section__title">Portfolio</h2>
             <div className="projects">
                 <div className="projects__item">
-                    <h3 className="project__title">Drinks Calculator</h3>
+                    <div className="projects__item__content">
+                        <Link to="/Drinks_Calculator" className="project__title">Drinks Calculator</Link>
+                    </div>
                 </div>
                 <div className="projects__item">
-                    <h3 className="project__title">Best Shop Website</h3>
+                    <div className="projects__item__content">
+                        <Link to="/Best_Shop" className="project__title">Best Shop Website</Link>
+                    </div>
                 </div>
                 <div className="projects__item">
-                    <h3 className="project__title">Capital City Game</h3>
+                    <div className="projects__item__content">
+                        <Link to="/Capital_City_Game" className="project__title">Capital City Game</Link>
+                    </div>
                 </div>
                 <div className="projects__item">
-                    <h3 className="project__title">Car Sharing Website</h3>
+                    <div className="projects__item__content">
+                        <Link to="/Car_Sharing" className="project__title">Car Sharing Website</Link>
+                    </div>
                 </div>
             </div>
         </section>
