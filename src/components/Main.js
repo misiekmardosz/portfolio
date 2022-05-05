1360cc11cce007632a4e08b9f3968e7c95042be9
misiekmardosz <michal.mardosz@gmail.com>
2022-05-05 11:13:58 +0200

    created main section for tablet

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import React from "react";
 const Main = () => {
     return(
         <section id="home" title="home" className="main container">
-            <div className="title_container">
+            <div className="title__container">
                 <h1 className="title">FRONT END DEVELOPER</h1>
             </div>
             <div className="graphic">
