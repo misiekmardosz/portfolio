14e777930a60ae99b3cf457d359aaf65fe740e96
misiekmardosz <michal.mardosz@gmail.com>
2022-05-11 11:30:45 +0200

    nosctript solving

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const container = document.getElementById('root');
+app.use(express.static(__dirname));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App tab="home" />);
 
