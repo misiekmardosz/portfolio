f5addb0c32947ac2a0518392e61903f1e4dc94d0
misiekmardosz <michal.mardosz@gmail.com>
2022-05-11 11:32:07 +0200

    changngin json

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-app.use(express.static(__dirname));
-
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App tab="home" />);
